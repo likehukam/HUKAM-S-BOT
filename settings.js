9804bc8e81423ac4517b1a98e62448a9c434dc0f
likehukam <113795428+likehukam@users.noreply.github.com>
2022-09-23 15:59:17 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -36,10 +36,10 @@ global.APIKeys = {
 
 
 //aumto functioner
-global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoTyping = true //auto tying in gc (true to on, false to off)
 global.autoreadpmngc = true //auto reading in gc and pm (true to on, false to off)
 global.autoReadGc = true //auto reading in gc (true to on, false to off)
-global.autoRecord = false //auto recording (true to on, false to off)
+global.autoRecord = true //auto recording (true to on, false to off)
 global.available = true //auto available (true to on, false to off)
 
 //semt menu variants
@@ -56,7 +56,7 @@ global.doc6 = 'application/vnd.android.package-archive'
 //owmner v card
 global.vcardowner = ['+923464022754'] //ur owner number
 global.ownername = "🦄Like Hukam" //ur owner name
-global.ytname = "🕊Mr.noone" //ur yt chanel name
+global.ytname = "🕊likehukam" //ur yt chanel name
 global.socialm = "likehukam" //ur github or insta name
 global.location = "villiage Ram Garh Hafizabad road Gujrawnwala punjab pakistan" //ur location
 
@@ -64,12 +64,12 @@ global.location = "villiage Ram Garh Hafizabad road Gujrawnwala punjab pakistan"
 global.owner = ['+923464022754']
 global.ownertag = '+923464022754' //ur tag number
 global.botname = 'HUKAMs BOT' //ur bot name
-global.linkz = "https://youtu.be/WiIqCdiDjFo" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
+global.linkz = "https://youtube.com/channel/UCe1N1yePO_gfifbX7ZZGiXg" //your theme url which will be displayed on whatsapp
+global.websitex = "https://instagram.com/likehukam?igshid=YmMyMTA2M2Y=" //ur website to be displayed
 global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
 global.reactmoji = "😘" //ur menu react emoji
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Likehhukam Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
+global.packname = "Likehukam Stickers\n\n\n\n\n\n\n\n\n\n\n🦄🦄🦄 " //ur sticker watermark packname
 global.author = "🦄Like Hukam" //ur sticker watermark author
 
 //Bot theme media
@@ -79,7 +79,7 @@ global.err4r = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur error pic
 global.thumb = fs.readFileSync("./XeonMedia/theme/cheemspic.jpg") //ur thumb pic
 
 //damtabase
-global.premium = ['923417709826'] //ur premium numbers
+global.premium = ['923464022754'] //ur premium numbers
 global.ntilinkytvid = []
 global.ntilinkytch = []
 global.ntilinkig = []
